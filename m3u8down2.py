33615c368bf6ce4c12e7402f5776d15643dd9ef4
Nchu <59404267+Nchujx@users.noreply.github.com>
2021-07-10 23:23:48 +0800

    Update m3u8down2.py

--- m3u8down2.py
+++ m3u8down2.py
@@ -1,4 +1,5 @@
 import re,os,time
+
 from ffmpy import FFmpeg
 from shutil import rmtree
 import requests
@@ -7,101 +8,26 @@ from Crypto.Cipher import AES
 from queue import Queue
 from threading import Thread
 import platform
+from argparse import ArgumentParser
 
-system = platform.system()
+###############
+# 初始化
 q = Queue(10000)
-
-title = ''
-count = 0
-enabledel = True
-
-preallsize = 0
-downsize = 0
-start = time.time()
-speed = 0
-Missions_completed = 0
-
+system = platform.system()
 headers = {
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36 Edg/91.0.864.59'
 }
 
-pad = lambda s: s + (16 - len(s)) * b"\0"
-# 获取m3u8文件基本信息
+downsize = 0
+Missions_completed = 0
+preallsize = 0
+time_start = time.time()
+
+######################
+
 class m3u8infos:
-    def __init__(self,m3u8,name,key,iv,m3u8BaseUrl,enableDel):
-        global title
-        global count
-        global enabledel
-        enabledel = enableDel
-
-        # http://****.m3u8
-        if ':\\' not in m3u8:
-            if name == '':
-                self.title = m3u8.split('?')[0].split('/')[-1].replace('.m3u8', '')
-            else:
-                self.title = name
-            requests.packages.urllib3.disable_warnings()
-            self.m3u8text = requests.get(url=m3u8, headers=headers,verify=False).text
-            self.m3u8BaseUrl = '/'.join(m3u8.split('?')[0].split('/')[:-1]) + '/'
-        # C:\Users\happy\Downloads\v.f230 (1).m3u8
-        else:
-            if name == '':
-                self.title = m3u8.split('\\')[-1].replace('.m3u8','')
-            else:
-                self.title = name
-        # \ / : * ？ " < > |
-            with open(f'{m3u8}','r') as f:
-                self.m3u8text = f.read()
-            self.m3u8BaseUrl = m3u8BaseUrl
-        self.title = re.sub(r"\\/:*?\"<>| ", "", self.title)[-64:]
-
-        if os.path.exists(self.title) == False:
-            os.makedirs(self.title)
-        self.tsurls = re.findall('#EXTINF.+\n(.+?)\n',self.m3u8text)
-        self.count = len(self.tsurls)
-        title = self.title
-        count = self.count
-        # 判读是否加密及加密类型
-        if 'METHOD=' in self.m3u8text:
-            self.method = re.findall('METHOD=(.+?),',self.m3u8text)[0]
-            self.key = key
-            self.iv = iv
-            if self.method == 'AES-128':
-                self.key = self.getkey(self.method)
-                self.iv = self.getiv(self.method)
-            elif self.method == 'SAMPLE-AES-CTR':
-                print('不支持 SAMPLE-AES-CTR 解密，下载完后自动二进制合并')
-                self.key = ''
-                self.iv = ''
-        else:
-            self.method = ''
-            self.key = ''
-            self.iv = ''
-
-
-        segments = []
-        for i in range(len(self.tsurls)):
-            segment = {
-                'title':self.title,
-                "index": i,
-                "count":self.count,
-                "method": self.method,
-                "key": self.key,
-                "iv": self.iv,
-                "segUrl": f'{self.m3u8BaseUrl}{self.tsurls[i]}' if 'http' not in self.tsurls[i] else f'{self.tsurls[i]}'
-            }
-            segments.append(segment)
-        self.meta = {
-            'm3u8':m3u8,
-            'm3u8BaseUrl':self.m3u8BaseUrl,
-            'm3u8Info':{
-                'count':self.count,
-                'segments':segments
-            }
-        }
-        print(f'{self.title} m3u8文件解析完成……')
-        with open(f'{self.title}/meta.json','w',encoding='utf-8') as f:
-            f.write(json.dumps(self.meta,indent=4))
+    def __init__(self):
+        pass
 
     def getkey(self,method):
         if self.key != '':
@@ -127,13 +53,95 @@ class m3u8infos:
             b64iv = base64.b64encode(iv).decode()
             return b64iv
 
-    def putsegments(self):
-        segments = self.meta['m3u8Info']['segments']
-        for segment in segments:
-            q.put(segment)
+    def check_title(self,title):
+        title = re.sub(r"\\/:*?\"<>| ", "", title)[-64:]
+        if os.path.exists(f'{title}.mp4'):
+            title += '(1)'
+            title = self.check_title(title)
+        return title
 
-# 多线程 下载ts 解密
+    def start(self,m3u8,name,key,iv,m3u8BaseUrl,enableDel,showLogs):
+        # http://****.m3u8
+        if ':\\' not in m3u8:
+            if name == '':
+                self.title = m3u8.split('?')[0].split('/')[-1].replace('.m3u8', '')
+            else:
+                self.title = name
+            requests.packages.urllib3.disable_warnings()
+            self.m3u8text = requests.get(url=m3u8, headers=headers, verify=False).text
+            self.m3u8BaseUrl = '/'.join(m3u8.split('?')[0].split('/')[:-1]) + '/'
+        # C:\Users\happy\Downloads\v.f230 (1).m3u8
+        else:
+            if name == '':
+                self.title = m3u8.split('\\')[-1].replace('.m3u8', '')
+            else:
+                self.title = name
+            # \ / : * ？ " < > |
+            with open(f'{m3u8}', 'r') as f:
+                self.m3u8text = f.read()
+            self.m3u8BaseUrl = m3u8BaseUrl
+        self.title = self.check_title(self.title)
+
+
+        if os.path.exists(self.title) == False:
+            os.makedirs(self.title)
+            os.makedirs(f'{self.title}/Part_0')
+        self.tsurls = re.findall('#EXTINF.+\n(.+?)\n', self.m3u8text)
+        self.count = len(self.tsurls)
+
+        # 判读是否加密及加密类型
+        if 'METHOD=' in self.m3u8text:
+            self.method = re.findall('METHOD=(.+?),', self.m3u8text)[0]
+            self.key = key
+            self.iv = iv
+            if self.method == 'AES-128':
+                self.key = self.getkey(self.method)
+                self.iv = self.getiv(self.method)
+            elif self.method == 'SAMPLE-AES-CTR':
+                print('不支持 SAMPLE-AES-CTR 解密，下载完后自动二进制合并')
+                self.key = ''
+                self.iv = ''
+        else:
+            self.method = ''
+            self.key = ''
+            self.iv = ''
+
+        segments = []
+        for i in range(len(self.tsurls)):
+            segment = {
+                'title':self.title,
+                "index": i,
+                "method": self.method,
+                "key": self.key,
+                "iv": self.iv,
+                "segUrl": f'{self.m3u8BaseUrl}{self.tsurls[i]}' if 'http' not in self.tsurls[i] else f'{self.tsurls[i]}'
+            }
+            segments.append(segment)
+        self.meta = {
+            'title': self.title,
+            'm3u8': m3u8,
+            'm3u8BaseUrl': self.m3u8BaseUrl,
+            'enableDel':enableDel,
+            'showLogs':showLogs,
+            'm3u8Info': {
+                'count': self.count,
+                'segments': segments
+            }
+        }
+        print(f'{self.title} m3u8文件解析完成……')
+        with open(f'{self.title}/meta.json', 'w', encoding='utf-8') as f:
+            f.write(json.dumps(self.meta, indent=4))
+        return self.meta
+
 class Consumer(Thread):
+    def __init__(self,title,count,retries,enableDel,showLogs):
+        Thread.__init__(self)
+        self.title = title
+        self.count = count
+        self.retries = retries
+        self.enableDel = enableDel
+        self.showLogs = showLogs
+
     def run(self):
         while True:
             # 检查队列中是否还有链接
@@ -142,18 +150,13 @@ class Consumer(Thread):
             self.ts_download(q.get())
 
     def ts_download(self, segment):
-        # 声明全局变量
-        global preallsize
         global downsize
         global Missions_completed
-        global start
-        global speed
+        global preallsize
+
         # 下载
         index = str(segment['index']).zfill(6)
-        title = segment['title']
-        count = segment['count']
-        retries = 16
-        for i in range(retries):
+        for i in range(self.retries):
             try:
                 requests.packages.urllib3.disable_warnings()
                 response = requests.get(url=segment['segUrl'], headers=headers, timeout=5,stream=True,verify=False)
@@ -161,8 +164,10 @@ class Consumer(Thread):
                 if response.status_code == 200:
                     break
             except:
-                print(f'\r\t{index}.ts requests failed {i+1} times.')
-                if i == 15:
+                if self.showLogs:
+                    print(f'\r\t{index}.ts requests failed {i + 1} times.')
+                time.sleep(1)
+                if i == self.retries-1:
                     ts = b''
 
         if segment['method'] == 'AES-128':
@@ -174,31 +179,23 @@ class Consumer(Thread):
             cryptor = AES.new(key=key, mode=AES.MODE_CBC,iv=iv)
 
             ts = cryptor.decrypt(ts)
-        if system == 'Linux':
-            if os.path.exists(f'{title}/Part_0') == False:
-                os.makedirs(f'{title}/Part_0')
-            with open(f'{title}/Part_0/{index}.ts', 'wb') as f:
-                f.write(ts)
-        elif system == 'Windows':
-            if os.path.exists(fr'{title}\Part_0') == False:
-                os.makedirs(fr'{title}\Part_0')
-            with open(fr'{title}\Part_0\{index}.ts', 'wb') as f:
-                f.write(ts)
+        with open(f'{self.title}/Part_0/{index}.ts', 'wb') as f:
+            f.write(ts)
 
         # 进度条
         downsize += ts.__sizeof__()
         Missions_completed += 1
-        preallsize = downsize * count / Missions_completed
+        preallsize = downsize * self.count / Missions_completed
         end = time.time()
-        speed = downsize / (end - start)
+        speed = downsize / (end - time_start)
         ETA = (preallsize - downsize) / speed
         m, s = divmod(int(ETA), 60)
         ETA = f'{m} m {s} s'
-        print(f'\r\tProcess: [{Missions_completed}/{count}]',
-              f'[{round(downsize / 1024 / 1024, 2)}Mb/{round(preallsize / 1024 / 1024, 2)}Mb]',
-              f'Speed={round(speed / 1024 / 1024, 2)}Mb/s', f'ETA={ETA}', end='')
+        mat = "{:18}{:30}{:18}{:20}"
+        print(mat.format(f"\r\tProcess:[{Missions_completed}/{self.count}]",f"[{round(downsize / 1024 / 1024, 2)}Mb/{round(preallsize / 1024 / 1024, 2)}Mb]", f"Speed={round(speed / 1024 / 1024, 2)}Mb/s",f'ETA={ETA}'),end='')
 
-        if Missions_completed == count:
+
+        if Missions_completed == self.count:
             # arg = 'copyb' if segment['method'] == 'SAMPLE-AES-CTR' else 'normal'
             self.combine()
 
@@ -209,61 +206,65 @@ class Consumer(Thread):
         :return:
         """
         print('\t视频合并中……',end='')
-        if system == 'Linux':
-            filelist = [fr"{os.path.abspath('')}/{title}/Part_0/{str(i).zfill(6)}.ts" for i in range(count)]
-            for ts_po in filelist:
-                with open(fr"{os.path.abspath('')}/{title}/{title}.ts", 'ab') as f:
-                    with open(ts_po, 'rb') as t:
-                        f.write(t.read())
-            # 检查合并是否完成
-            if os.path.exists(fr"{os.path.abspath('')}/{title}/{title}.ts") == True:
-                print('\t合并完成！', end='')
-                print('视频转码……\t', end='')
-                self.ffmpeg(input_path=fr'{os.path.abspath("")}/{title}/{title}.ts',
-                            output_path=fr'{os.path.abspath("")}/{title}.mp4')
-                print('转码成功！\t', end='')
+        filelist = [fr"{os.path.abspath('')}/{self.title}/Part_0/{str(i).zfill(6)}.ts" for i in range(self.count)]
 
-        elif system == 'Windows':
-            filelist = [fr"{os.path.abspath('')}\{title}\Part_0\{str(i).zfill(6)}.ts" for i in range(count)]
-            for ts_po in filelist:
-                with open(fr"{os.path.abspath('')}\{title}\{title}.ts", 'ab') as f:
-                    with open(ts_po, 'rb') as t:
-                        f.write(t.read())
-            # 检查合并是否完成
-            if os.path.exists(fr"{os.path.abspath('')}\{title}\{title}.ts") == True:
-                print('\t合并完成！', end='')
+        for ts_po in filelist:
+            with open(fr"{os.path.abspath('')}/{self.title}/{self.title}.ts", 'ab') as f:
+                with open(ts_po, 'rb') as t:
+                    f.write(t.read())
+        # 检查合并是否完成
+        if os.path.exists(fr"{os.path.abspath('')}/{self.title}/{self.title}.ts") == True:
+            print('\t合并完成！', end='')
+            print('\t视频转码……', end='')
+            self.ffmpeg(input_path=fr'{os.path.abspath("")}/{self.title}/{self.title}.ts',
+                        output_path=fr'{os.path.abspath("")}/{self.title}.mp4')
+            print('\t转码成功！', end='')
 
-                print('视频转码……\t', end='')
-                self.ffmpeg(input_path=fr'{os.path.abspath("")}\{title}\{title}.ts',
-                            output_path=fr'{os.path.abspath("")}\{title}.mp4')
-                print('转码成功！')
-
-        if enabledel == True:
+        if self.enableDel == True:
             self.del_after_done()
 
     def ffmpeg(self,input_path,output_path):
         ff = FFmpeg(inputs={input_path: None}, outputs={output_path: '-c copy -loglevel panic'})
-        # print(ff.cmd)
         ff.run()
 
     def del_after_done(self):
-        rmtree(rf'{os.path.abspath("")}\{title}', ignore_errors=True)
-        if os.path.exists(rf'{os.path.abspath("")}\{title}') == False:
-            print('删除分片成功！')
+        rmtree(rf'{os.path.abspath("")}\{self.title}', ignore_errors=True)
+        if os.path.exists(rf'{os.path.abspath("")}\{self.title}') == False:
+            print('\t删除分片成功！')
 
-def run(m3u8,name='',b64key='',b64iv='',enableDel=True,m3u8BaseUrl=''):
-    # m3u8infos(m3u8, name, b64key, b64iv, m3u8BaseUrl, enableDel).putsegments()
-    try:
-        m3u8infos(m3u8, name, b64key, b64iv, m3u8BaseUrl, enableDel).putsegments()
-    except:
-        print('Error in reading file.')
-    #     sys.exit(0)
-    for i in range(16):
-        t = Consumer()
+def run(m3u8,name='',b64key='',b64iv='',enableDel=True,m3u8BaseUrl='',showLogs=False,Threads=16,retries=16):
+    meta = m3u8infos().start(m3u8, name, b64key, b64iv, m3u8BaseUrl, enableDel,showLogs)
+
+    title = meta['title']
+    count = meta['m3u8Info']['count']
+
+    segments = meta['m3u8Info']['segments']
+    for segment in segments:
+        q.put(segment)
+    print(f"\r\tProcess:[0/]    [/]                           Speed= Mb/s        ETA= m  s",end='')
+    for i in range(Threads):
+        t = Consumer(title,count,retries,enableDel,showLogs)
         t.start()
 
 if __name__ == '__main__':
-    m3u8 = r'https://apd-ffd5b6848ea656ba3ea922752cd01391.v.smtcdns.com/omts.tc.qq.com/AHp8xEH5-04o_vEqU_jjae359DCZH6QHmOK7QTc4yruE/uwMROfz2r55goaQXGdGnC2de64gtX89GT746tcTJVnDpJgsD/svp_50112/3tA3bGPTljbpqRfWDbdXnwcVb3lmfsdw3jG3CDZtQrTcicALPXgDPvuXkpxMYHbRSFSJ4gLhe04U-CY9SfrYdczq8qOBKDCkgWv23kXlXpq531FLNEMji_NzzKqJYzjxa14S2rjXy1wIxRdAMuvIhRuxmYJYQCroO5VZCH0cd4DmQQ0P38sklg/gzc_1000102_0b53dyabyaaatyaihi7cnfqmahwddqmaagca.f321004.ts.m3u8?ver=4'
-    run(m3u8=m3u8, name='', b64key='',m3u8BaseUrl='')
+    parser = ArgumentParser(
+        prog="https://github.com/Nchujx/m3u8down",
+        description=("M3U8 streaming video download script,this is python version.")
+    )
+    parser.add_argument("-m3u8", help="视频地址：网络链接或本地文件链接")
+    parser.add_argument("-name", default='', help="视频名称")
+    parser.add_argument("-b64key", default='', help="自定义key:base64编码")
+    parser.add_argument("-b64iv", default='', help="自定义iv:base64编码")
+    parser.add_argument("-enableDel", default=True, help="下载完后删除多余文件", type=bool)
+    parser.add_argument("-m3u8BaseUrl", default='', help="链接前缀:用于补全链接")
+    parser.add_argument("-showLogs", default=False, help="显示错误日志")
+    parser.add_argument("-Threads", default=16, help="线程数")
+    parser.add_argument("-retries", default=16, help="尝试重试次数")
+    args = parser.parse_args()
+    run(m3u8=args.m3u8, name=args.name, b64key=args.b64key, b64iv=args.b64iv, enableDel=args.enableDel,
+        m3u8BaseUrl=args.m3u8BaseUrl, showLogs=args.showLogs, Threads=args.Threads, retries=args.retries)
+
+    # m3u8 = 'https://1252524126.vod2.myqcloud.com/9764a7a5vodtransgzp1252524126/987bb91f5285890799770660639/drm/v.f230.m3u8'
+    # run(m3u8=m3u8, name='', b64key='',b64iv='',enableDel=True,m3u8BaseUrl='',showLogs=False,Threads=16,retries=16)
 
 
